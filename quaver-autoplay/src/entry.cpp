3b7e6f3c11f05dd9d16067801a181fd750bd3483
lanylow <lanylow@protonmail.com>
2021-10-28 20:18:21 +0200

    Base commit.

---
+++ quaver-autoplay/src/entry.cpp
@@ -0,0 +1,11 @@
+#include "include.hpp"
+
+void autoplayer_thread() {
+
+}
+
+bool __stdcall DllMain(void *instance, int reason, void *reserved) { 
+	if (reason == DLL_PROCESS_ATTACH)
+    std::thread(autoplayer_thread).detach();
+	return true;
+}
