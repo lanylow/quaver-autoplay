8141d27e0e0e2184668e7ab99be80b8a4a5f54ce
lanylow <lanylow@protonmail.com>
2021-10-28 20:18:57 +0200

    Indent fix.

--- quaver-autoplay/src/entry.cpp
+++ quaver-autoplay/src/entry.cpp
@@ -1,11 +1,9 @@
 #include "include.hpp"
 
-void autoplayer_thread() {
+void autoplayer_thread() {}
 
-}
-
-bool __stdcall DllMain(void *instance, int reason, void *reserved) { 
-	if (reason == DLL_PROCESS_ATTACH)
+bool __stdcall DllMain(void *instance, int reason, void *reserved) {
+  if (reason == DLL_PROCESS_ATTACH)
     std::thread(autoplayer_thread).detach();
-	return true;
+  return true;
 }
